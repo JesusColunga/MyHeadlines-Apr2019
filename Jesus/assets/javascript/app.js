caa86c558197aac1dc746fff0de5d4b86ab2d5ff
JesusColunga <47442182+JesusColunga@users.noreply.github.com>
2019-04-06 15:09:40 -0600

    6/Abr/2019 15:09

--- Jesus/assets/javascript/app.js
+++ Jesus/assets/javascript/app.js
@@ -1,10 +1,14 @@
-/* app.js                */
-/*  GifTastic: GIPHY API */
-/* 23/Mar/2019           */
+/* app.js               */
+/*  TheGuardian API     */
+/* 6/Apr/2019           */
 
 
 // GLOBAL VARIABLES
 // =======================================================================================
+var queryURL    = "https://content.guardianapis.com";
+var queryParams = "/search?q=";
+var apiKey      = "d29910de-c99f-41a2-936b-c52c08761666";
+var q = queryURL + queryParams + "mexico&api-key=" + apiKey;
 
 
 // OBJECTS
@@ -13,9 +17,45 @@
 
 // FUNCTIONS (Definition)
 // =======================================================================================
+function muestra (results) {
+	var r = $("#row1");
+	var sp, br;
+	console.log ("results", results);
+	console.log ("long", results.length);
+	for (ct = 0; ct < results.length; ct ++) {
+		sp = $("<p>");
+		br = $("<br>");
+		sp.text ( results [ct].webTitle );
+		r.append ( sp, br );
+	};
+};
 
 
 // FUNCTION CALLS (Execution)
 // =======================================================================================
 $(document).ready(function() {
+  
+  $.ajax(
+		{url    : q,
+		 method : "GET"}
+	).done ( 
+		function (data) {   // Se ejecuta después de traer la info del servidor
+			  //console.log (data);
+			  muestra ( data.response.results );
+		});
+  
 }); // document.ready
+
+/*
+	$.ajax ( {url:queryURL, method: 'GET'} )
+		  .done ( function (response) {   // Se ejecuta después de traer la info del servidor
+			  console.log (response);
+			  $(".container").append (JSON.stringify (response) );
+		  });
+---------------
+  $.ajax(
+	{url    : q,
+     method : "GET"}
+	).then(updatePage);
+
+*/
